b9b256f0b4f4fedb914ffc3926ed07b52e14314c
Derek Nedveck <nedveck@gmail.com>
2019-01-28 18:04:49 -0500

    still trying to get the AWK statement to work

--- 03_blast-query-against-gencode.sh
+++ 03_blast-query-against-gencode.sh
@@ -2,12 +2,12 @@ mkdir -p blast-results
 
 blastn -db gencode-refs/gencode_db -query query-sequence.fa -out blast-results/results.txt
 
-hits=`cat blast-results/results.txt`
+hits=`cat blast-results/results.txt | grep '>' | uniq | cut -f 1 -d'|' | sed -e 's/>//g'`
+
 
 for hit in $hits
 do
-	awk 'BEGIN{ ORS = ""; RS = ">"; FS="\n" } $1 == $hit \
-		{ print ">" $0 }' gencode-refs/gencode_txome.fa	\
-			>> blast-results/results.fa
+	awk 'BEGIN{RS=">";FS="\n"}NR>1{if ($1~/${hit}/) print ">"$0}' gencode-refs/gencode_txome.fa 
+	#>> blast-results/results.fa
 done
 
