45ad0db875b693925a58d1405bb4ba7c04f2c45b
Derek Nedveck <nedveck@gmail.com>
2019-01-28 15:15:06 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This short little project is practice for getting putative transcripts out of a de novo transcriptome. 
+
+I'm going to be using the gencode v29 transcript + ncrna sequences to build a blast DB, create a query string from some exons of a gene, and then see what kind of transcripts I get from blasting it. 
---
+++ download-gencode-fasta.sh
@@ -0,0 +1,11 @@
+mkdir -p gencode-refs
+
+cd gencode-refs
+
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_29/gencode.v29.transcripts.fa.gz
+
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_29/gencode.v29.lncRNA_transcripts.fa.gz
+
+cat gencode.v29.transcripts.fa.gz gencode.v29.lncRNA_transcripts.fa.gz > gencode_txome.fa.gz
+
+cd ..
